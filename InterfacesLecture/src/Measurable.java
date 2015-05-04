3c9ee95a5818b30747ac8535e1b986d533ef05ca
Doug Shook <dsshook@gmail.com>
2015-05-04 11:20:45 -0500

    pset3 and lecture

---
+++ InterfacesLecture/src/Measurable.java
@@ -0,0 +1,19 @@
+/**
+ * Created by dshook on 5/4/15.
+ *
+ * Interfaces:
+ *  Definition
+ *  Implementation
+ *  Comparison to inheritance
+ *  Polymorphism
+ * Exceptions
+ *  try/catch
+ *  throwing
+ *  data validation
+ *  creating your own exceptions
+ */
+public interface Measurable {
+
+    public double getArea();
+    public double getPerimeter();
+}
---
+++ pset3b/src/README.md
@@ -0,0 +1,5 @@
+#More practice with Exceptions
+
+Revisit the previous problem sets (some good candidates would be pset1 and pset2b) and add data validation / exception handling.
+
+Test your new code to make sure it behaves in the way you expect.
